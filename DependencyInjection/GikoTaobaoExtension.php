ffdf52c8fdc53d83ad08d50bf5640abd407c41e2
Giko <wo@luochunhui.com>
2013-05-29 23:41:03 +0800

    add options

--- DependencyInjection/GikoTaobaoExtension.php
+++ DependencyInjection/GikoTaobaoExtension.php
@@ -2,9 +2,11 @@
 
 namespace Giko\TaobaoBundle\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Processor;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
 /**
@@ -19,34 +21,21 @@ class GikoTaobaoExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-    	$processor = new Processor();
     	$configuration = new Configuration();
-    	$config = $processor->processConfiguration($configuration, $configs);
-    	
-    	$this->loadDefaults($container);
-    	
-    	if (isset($config['alias'])) {
-    		$container->setAlias($config['alias'], 'giko_taobao.service');
-    	}
-    	
-    	foreach (array('libpath', 'app_key', 'app_secret', 'api_url', 'pid') as $attribute) {
-    		if (isset($config[$attribute])) {
-    			$container->setParameter('giko_taobao.'.$attribute, $config[$attribute]);
-    		}
-    	}
-    	$path = $config['libpath'];
-    	set_include_path(get_include_path() . PATH_SEPARATOR . $path);
-    }
-    
-    /**
-     * @codeCoverageIgnore
-     */
-    protected function loadDefaults($container)
-    {
-    	$loader = new XmlFileLoader($container, new FileLocator(array(__DIR__.'/../Resources/config', __DIR__.'/Resources/config')));
-    
-    	foreach ($this->resources as $resource) {
-    		$loader->load($resource);
-    	}
+        $config = $this->processConfiguration($configuration, $configs);
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
+        
+        if (isset($config['alias'])) {
+        	$container->setAlias($config['alias'], 'giko_taobao.service');
+        }
+        
+        foreach (array('libpath', 'app_key', 'app_secret', 'api_url', 'pid') as $attribute) {
+        	if (isset($config[$attribute])) {
+        		$container->setParameter('giko_taobao.'.$attribute, $config[$attribute]);
+        	}
+        }
+        $path = $config['libpath'];
+        set_include_path(get_include_path() . PATH_SEPARATOR . $path);
     }
 }
